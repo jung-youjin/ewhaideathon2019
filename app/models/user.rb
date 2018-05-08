9ce1dba9237f986bd74887a6000d219636946242
chanha22 <sgu960401@sogang.ac.kr>
2018-05-08 22:44:18 +0900

    user table 설정

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,15 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
-  belongs_to :posts
+
+  has_many :posts
   has_many :comments
   has_many :likes
   has_many :liked_posts, through: :likes, source: :users
 
 
   def is_like?(post)
-   Like.find_by(user_id: self.id, post_id: post.id).present? 
+   Like.find_by(user_id: self.id, post_id: post.id).present?
   end
   #User 모델의 한 객체에 대해 특정 글에 대한 좋아요 유무를 확인하는 메소드
 
