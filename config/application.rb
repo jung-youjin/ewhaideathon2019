be1a1280019f055048c8f25b520bfc805cd6e269
chanha22 <sgu960401@sogang.ac.kr>
2018-05-08 23:10:42 +0900

    adsfasdfasdf

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module Workspace
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.initialize_on_precompile = false
-    config.autoload_paths += Dir[Rails.root.join('app', 'policies', '*.rb')]
+    config.autoload_paths += Dir[Rails.root.join('app/policies/*.rb')].each &method(:require)
+
   end
 end
