afb4fa80221f89a0f1e7019e180e11b0bb04c8e4
JeonJeaWon <aapple0525@naver.com>
2019-05-16 09:37:18 +0000

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180506112937) do
+ActiveRecord::Schema.define(version: 20180508135004) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "name"
